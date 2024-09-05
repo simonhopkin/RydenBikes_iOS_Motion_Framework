975944f057d303ccab39e7df8c65e35c5f0b4ea2
Simon Hopkin <shopkin@nemaura.co.uk>
2024-09-05 10:21:32 +0100

    Initial Commit

---
+++ Motion/Motion.h
@@ -0,0 +1,18 @@
+//
+//  Motion.h
+//  Motion
+//
+//  Created by Simon Hopkin on 05/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Motion.
+FOUNDATION_EXPORT double MotionVersionNumber;
+
+//! Project version string for Motion.
+FOUNDATION_EXPORT const unsigned char MotionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Motion/PublicHeader.h>
+
+
